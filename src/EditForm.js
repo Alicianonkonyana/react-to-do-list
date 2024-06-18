b4290df67d4d43e48ffa7bb9b8871608a2267dab
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-06-18 09:57:29 +0200

    used States to clear the form after submitting the input value

--- src/EditForm.js
+++ src/EditForm.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 export const EditForm = () => {
   return <div>EditForm</div>;
 };
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,9 +1,20 @@
-import React from "react";
-export const TodoForm = () => {
+import React, { useState } from "react";
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
   return (
     <div className="TodoForm">
-      <form>
-        <input type="text" placeholder="What is the task"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="What is the task today?"
+          value={value}
+        ></input>
         <button type="submit">Add task</button>
       </form>
     </div>
